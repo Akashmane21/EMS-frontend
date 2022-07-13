b971c0cd345e275f2c6e822385e4b7b3a114b3bf
Akash Mane <akash21mane21@gmail.com>
2022-07-13 18:40:47 +0530

    last one

--- src/EmployeeDashboard/Salary_details.js
+++ src/EmployeeDashboard/Salary_details.js
@@ -36,68 +36,70 @@ export default function Salary_details() {
   return (
     <div>
       
-                  <Menu />
-            <div className="rightbody" >
+               
+            {/* <div className="rightbody" > */}
 
-                <BredNav name="Salary Details" />
+                {/* <BredNav name="Salary Details" /> */}
                 
-            <h1>Salary Details - Employee</h1>
+            <h1 
+            style={{ padding:20 , fontFamily:"serif" }}
+            >Salary Slip - </h1>
             {Data &&
             <>
             <dl class="row info m-3">
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         month
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
         {Data.month}
         </dd>
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         year
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
             {Data.year}
         </dd>
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         basic_Pay : 
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
             {Data.basic_Pay}
         </dd>
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         hra
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
             {Data.hra}
         </dd>
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         conveyance_Allowences
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
             {Data.conveyance_Allowences}
         </dd>
        
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         special_Allowences:
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
             {Data.special_Allowences}
         </dd>
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         pf
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
             {Data.tds}
         </dd>
-         <dt class = "col-sm-5">
+         <dt class = "col-sm-3">
          gross_Salary
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
            {Data.gross_Salary}
         </dd>
-        <dt class = "col-sm-5">
+        <dt class = "col-sm-3">
         net_Salary
         </dt>
-        <dd class = "col-sm-10">
+        <dd class = "col-sm-3">
            {Data.net_Salary}
         </dd>
        
@@ -106,7 +108,7 @@ export default function Salary_details() {
     </> 
 }
 
-            </div>
+            {/* </div> */}
     </div>
   )
 }
